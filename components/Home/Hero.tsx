c1a82f905022436f07e8be579c091aa6259ec053
BalaAyyappan1 <balaayyappan11@gmail.com>
2025-04-02 17:20:25 +0530

    text color

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -11,7 +11,7 @@ const Hero = () => {
           <TopNav />
       </div>
 
-      <div className='absolute md:bottom-15 md:left-20 bottom-15 left-10 font-medium text-[20px] md:text-[91px] md:leading-[112px]'>
+      <div className='absolute md:bottom-15 md:left-20 bottom-15 left-10 font-medium text-[20px] md:text-[91px] md:leading-[112px] text-white'>
       Turnkey Contracting | Woodwork Mastery |  Timeless Interiors   
       </div>
     </div>
